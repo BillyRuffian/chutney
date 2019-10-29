d0336fa867cfa87acfc35bd3370aa17fefed78e9
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2019-10-29 16:28:28 +0000

    max step length is now a configurable value

--- lib/chutney/linter/too_long_step.rb
+++ lib/chutney/linter/too_long_step.rb
@@ -1,17 +1,29 @@
 require 'chutney/linter'
 
+# rubocop:disable Lint/MissingCopEnableDirective
 module Chutney
   # service class to lint for too long steps
   class TooLongStep < Linter
+    
+    def initialize
+      @maxlength = 80
+      super
+    end
+  
     MESSAGE = 'This step is too long at %d characters'.freeze
     
     def lint
       steps do |file, feature, scenario, step|
-        next if step[:text].length < 80
+        next if step[:text].length <= @maxlength
         
         references = [reference(file, feature, scenario, step)]
         add_error(references, MESSAGE % step[:text].length)
       end
+    end
+    
+    # rubocop:disable Style/TrivialAccessors
+    def maxlength(length)
+      @maxlength = length
     end
   end
 end
