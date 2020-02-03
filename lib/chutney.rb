c6ffb4600e7424001c9133f9bb41e8b67febdf78
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2020-02-03 15:55:19 +0000

    remove the old junk;

--- lib/chutney.rb
+++ lib/chutney.rb
@@ -36,6 +36,7 @@ require 'gherkin/dialect'
 require 'gherkin/parser'
 require 'i18n'
 require 'set'
+require 'yaml'
 
 module Chutney
   # gherkin linter
@@ -46,21 +47,19 @@ module Chutney
     attr_reader :results
     
     def_delegators :@files, :<<, :clear, :delete, :include?
-    
-    #     default_file = File.expand_path('..', __dir__), '**/config', 'default.yml'
-    #     DEFAULT_CONFIG = Dir.glob(File.join(default_file)).first.freeze
-    #     LINTER = Linter.descendants
 
     def initialize(*files)
       @files = files
       @results = Hash.new { |h, k| h[k] = [] }
-      I18n.load_path << Dir[File.expand_path(File.join(__dir__, 'config/locales')) + '/*.yml']
-      I18n.default_locale = :en # (note that `en` is already the default!)
+      i18n_paths = Dir[File.expand_path(File.join(__dir__, 'config/locales')) + '/*.yml']
+      return if I18n.load_path.include?(i18n_paths)
+
+      I18n.load_path << i18n_paths
     end
     
     def configuration
       unless @config
-        default_file = File.expand_path('..', __dir__), '**/config', 'chutney.yml'
+        default_file = [File.expand_path('..', __dir__), '**/config', 'chutney.yml']
         config_file = Dir.glob(File.join(default_file)).first.freeze
         @config = Configuration.new(config_file)
       end
@@ -71,55 +70,6 @@ module Chutney
       @config = config
     end
     
-    # 
-    #     def enabled(linter_name, value)
-    #       @config.config[linter_name]['Enabled'] = value if @config.config.key? linter_name
-    #     end
-    # 
-    #     def enable(enabled_linters)
-    #       enabled_linters.each do |linter|
-    #         enabled linter, true
-    #       end
-    #     end
-    # 
-    #     def disable(disabled_linters)
-    #       disabled_linters.each do |linter|
-    #         enabled linter, false
-    #       end
-    #     end
-    # 
-    #     # Testing feature
-    #     def disable_all
-    #       @config.config.each do |member|
-    #         @config.config[member[0]]['Enabled'] = false
-    #       end
-    #     end
-    # 
-    #     def set_linter
-    #       @linter = []
-    #       LINTER.each do |linter|
-    #         new_linter = linter.new
-    #         linter_enabled = @config.config[new_linter.class.name.split('::').last]['Enabled']
-    #         evaluate_members(new_linter) if linter_enabled
-    #         @linter.push new_linter if linter_enabled
-    #       end
-    #     end
-    # 
-    #     def evaluate_members(linter)
-    #       @config.config[linter.class.name.split('::').last].each do |member, value|
-    #         next if member.downcase.casecmp('enabled').zero?
-    #         
-    #         member = member.downcase.to_sym
-    #         raise 'Member not found! Check the YAML' unless linter.respond_to? member
-    #         
-    #         linter.public_send(member, value)
-    #       end
-    #     end
-    # 
-    #     def analyze(file)
-    #       @files[file] = parse file
-    #     end
-
     def analyse
       files.each do |f|
         lint(f)
@@ -154,38 +104,5 @@ module Chutney
         @results[file] << { linter: linter.linter_name, issues: linter.issues }
       end
     end
-
-    # 
-    #     def parse(file)
-    #       to_json File.read(file)
-    #     end
-    # 
-    #     def report
-    #       issues = @linter.map do |linter|
-    #         linter.lint_files(@files, disable_tags)
-    #         linter.issues
-    #       end.flatten
-    # 
-    #       print issues
-    #       return 0 if issues.select { |issue| issue.class == Error }.empty?
-    #       
-    #       -1
-    #     end
-    # 
-    #     def disable_tags
-    #       LINTER.map { |lint| "disable#{lint.new.class.name.split('::').last}" }
-    #     end
-    # 
-    #     def to_json(input)
-    #       parser = Gherkin::Parser.new
-    #       scanner = Gherkin::TokenScanner.new input
-    # 
-    #       parser.parse(scanner)
-    #     end
-    # 
-    #     def print(issues)
-    #       puts 'There are no issues' if issues.empty? && @verbose
-    #       issues.each { |issue| puts issue.render }
-    #     end
   end
 end
