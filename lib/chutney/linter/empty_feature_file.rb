8a96afc0b2345751fa6be291140b2cf1821e7ca1
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2020-08-25 15:01:09 +0100

    allowing issues to be raised without a feature -- this is specifically to support the linter for empty files

--- lib/chutney/linter/empty_feature_file.rb
+++ lib/chutney/linter/empty_feature_file.rb
@@ -2,7 +2,7 @@ module Chutney
   # service class to lint for Features that have no content
   class EmptyFeatureFile < Linter
     def lint
-      add_issue(I18n.t('linters.empty_feature_file'), { name: @filename.split('/').last }) if feature.nil?
+      add_issue(I18n.t('linters.empty_feature_file')) if feature.nil?
     end
   end
 end
