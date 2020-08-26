a9f69a704e3c104cab602293904c22808c1125db
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2020-08-26 10:34:48 +0100

    file name differs ported

--- lib/chutney/linter/file_name_differs_feature_name.rb
+++ lib/chutney/linter/file_name_differs_feature_name.rb
@@ -2,10 +2,10 @@ module Chutney
   # service class to lint for file name differs feature name
   class FileNameDiffersFeatureName < Linter
     def lint
-      return unless feature&.include?(:name)
+      return unless feature
         
       expected_feature_name = title_case(filename)
-      return if ignore_whitespaces(feature[:name]).casecmp(ignore_whitespaces(expected_feature_name)) == 0
+      return if ignore_whitespaces(feature.name).casecmp(ignore_whitespaces(expected_feature_name)) == 0
         
       add_issue(I18n.t('linters.file_name_differs_feature_name', expected: expected_feature_name), feature) 
     end
