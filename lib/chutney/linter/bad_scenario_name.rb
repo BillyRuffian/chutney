e1ef14575f5772624f8a1d82957c6f8d11bcfab4
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2020-08-26 10:33:11 +0100

    bad scenario name ported

--- lib/chutney/linter/bad_scenario_name.rb
+++ lib/chutney/linter/bad_scenario_name.rb
@@ -3,10 +3,10 @@ module Chutney
   class BadScenarioName < Linter
     def lint
       scenarios do |feature, scenario|
-        next if scenario[:name].empty?
+        next if scenario.name.empty?
         
         bad = /\w*(test|verif|check)\w*/i
-        match = scenario[:name].match(bad).to_a.first
+        match = scenario.name.match(bad).to_a.first
         add_issue(I18n.t('linters.bad_scenario_name', word: match), feature, scenario) if match
       end
     end
