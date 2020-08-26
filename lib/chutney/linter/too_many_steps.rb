bfb843ed67ea7a4d37819c2c860ae21fd586336d
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2020-08-26 11:51:09 +0100

    too many steps ported

--- lib/chutney/linter/too_many_steps.rb
+++ lib/chutney/linter/too_many_steps.rb
@@ -3,10 +3,10 @@ module Chutney
   class TooManySteps < Linter
     def lint
       filled_scenarios do |feature, scenario|
-        next if scenario[:steps].length <= maxcount
+        next if scenario.steps.length <= maxcount
         
         add_issue(
-          I18n.t('linters.too_many_steps', count: scenario[:steps].length, max: maxcount), 
+          I18n.t('linters.too_many_steps', count: scenario.steps.length, max: maxcount), 
           feature
         )
       end
