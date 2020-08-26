3f0f757ff761a0440ad39247e27d89eb45039bae
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2020-08-26 11:10:09 +0100

    missing verification

--- lib/chutney/linter/missing_test_action.rb
+++ lib/chutney/linter/missing_test_action.rb
@@ -3,7 +3,7 @@ module Chutney
   class MissingTestAction < Linter
     def lint
       filled_scenarios do |feature, scenario|
-        when_steps = scenario[:steps].select { |step| when_word?(step[:keyword]) }
+        when_steps = scenario.steps.select { |step| when_word?(step.keyword) }
         next unless when_steps.empty?
         
         add_issue(I18n.t('linters.missing_test_action'), feature, scenario)
--- lib/chutney/linter/missing_verification.rb
+++ lib/chutney/linter/missing_verification.rb
@@ -5,7 +5,7 @@ module Chutney
   class MissingVerification < Linter
     def lint
       filled_scenarios do |feature, scenario|
-        then_steps = scenario[:steps].select { |step| then_word?(step[:keyword]) }
+        then_steps = scenario.steps.select { |step| then_word?(step.keyword) }
         next unless then_steps.empty?
         
         add_issue(I18n.t('linters.missing_test_verification'), feature, scenario)
