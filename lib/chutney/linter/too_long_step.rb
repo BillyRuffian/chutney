0ce8b5a06c433454a9b336d26ac31cc92d657eca
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2020-08-26 11:49:30 +0100

    too long step ported

--- lib/chutney/linter/too_long_step.rb
+++ lib/chutney/linter/too_long_step.rb
@@ -3,10 +3,10 @@ module Chutney
   class TooLongStep < Linter
     def lint
       steps do |feature, scenario, step|
-        next if step[:text].length <= maxlength
+        next if step.text.length <= maxlength
         
         add_issue(
-          I18n.t('linters.too_long_step', length: step[:text].length, max: maxlength), 
+          I18n.t('linters.too_long_step', length: step.text.length, max: maxlength), 
           feature, scenario
         )
       end
