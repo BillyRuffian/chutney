93d9766b6f09e4d38aab32e175747773f10065b0
Abdi Warsame <abdi.warsame@dvla.gov.uk>
2022-08-25 11:16:56 +0100

    created linter for avoid splat in background

---
+++ lib/chutney/linter/avoid_splat_steps_in_background.rb
@@ -0,0 +1,14 @@
+module Chutney
+  # service class to lint for avoiding periods
+  class AvoidSplatStepsInBackground < Linter
+    def lint
+      background do |feature, background|
+        splat_steps = background&.steps&.select { |s| s.keyword == '*' }
+        # add_issue(I18n.t('linters.avoid_full_stop'), feature, child, step) if step.text.strip.end_with? '.'
+        if splat_steps && !splat_steps.empty?
+          add_issue(I18n.t('linters.avoid_splat_steps_in_background'), feature, background, splat_steps.first)
+        end
+      end
+    end
+  end
+end
