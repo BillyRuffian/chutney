66ae08f03a52dcf99a5841039dac3beacc964da5
Kieran Ayres <Kieran.Ayres@dvla.gov.uk>
2025-08-20 14:07:18 +0100

    bugfix: return to the method if the feature is nil

--- lib/chutney/linter/avoid_colons_at_start_of_names.rb
+++ lib/chutney/linter/avoid_colons_at_start_of_names.rb
@@ -4,9 +4,9 @@ module Chutney
   # service class to lint for avoiding colons at the start of names
   class AvoidColonsAtStartOfNames < Linter
     def lint
-      unless feature.nil?
-        add_issue(I18n.t('linters.avoid_colons_at_start_of_names'), feature) if feature.name.start_with? ':'
-      end
+      return if feature.nil?
+
+      add_issue(I18n.t('linters.avoid_colons_at_start_of_names'), feature) if feature.name.start_with? ':'
 
       scenarios do |_feature, scenario|
         if scenario.name.start_with? ':'
